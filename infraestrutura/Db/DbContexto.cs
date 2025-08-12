9772179440b69ede6bd78c543865ac32f8b2da7a
Taly <taly.oliv@gmail.com>
2025-08-12 14:31:29 -0300

    regras para autorização

--- infraestrutura/Db/DbContexto.cs
+++ infraestrutura/Db/DbContexto.cs
@@ -1,6 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalApi.Dominio.Entidades;
+
 namespace MinimalApi.Infraestrutura.Db;
 
-public class DBContexto
+public class DBContexto : DbContext
 {
-    
+    private readonly IConfiguration _configuracaoAppSettings;
+    public DBContexto(IConfiguration configuracaoAppSettin)
+    {
+        _configuracaoAppSettings = configuracaoAppSettin;
+    }
+    public DbSet<Administrador> Administradores { get; set; } = default!;
+
+    public DbSet<Veiculo> Veiculos { get; set; } = default!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Administrador>().HasData(
+            new Administrador
+            {
+                Id = 1,
+                Email = "administrador@teste.com",
+                Senha = "123456",
+                Perfil = "Adm"
+            }
+        );
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            var stringConexao = _configuracaoAppSettings.GetConnectionString("MySql")?.ToString();
+            if (!string.IsNullOrEmpty(stringConexao))
+            {
+                optionsBuilder.UseMySql(stringConexao, ServerVersion.AutoDetect(stringConexao));
+            }
+        }
+    }
 }
